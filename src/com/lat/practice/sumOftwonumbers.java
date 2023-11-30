860e33d84349bfe7d825964fcf02cdbc5038da3e
nandareddy431 <nandareddy375@gmail.com>
2023-11-30 19:21:45 +0530

    commiting to branch

---
+++ src/com/lat/practice/sumOftwonumbers.java
@@ -0,0 +1,10 @@
+package com.lat.practice;
+public class sumOftwonumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+System.out.println("please merge the code");
+
+}
+}
